32d2a2d5a2ee8937c5fd5d9f407ff809b12b6c46
Alvin <912806786@qq.com>
2026-02-18 10:34:24 +0800

    Initial Commit

---
+++ CircaAlarm/CircaAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircaAlarmApp.swift
+//  CircaAlarm
+//
+//  Created by 牛慧升 on 2026/2/18.
+//
+
+import SwiftUI
+
+@main
+struct CircaAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CircaAlarm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CircaAlarm
+//
+//  Created by 牛慧升 on 2026/2/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
